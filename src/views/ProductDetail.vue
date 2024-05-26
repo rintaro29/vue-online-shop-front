16b8f327d2d535ccf59d6b22485a5f9810c122ed
rintaro1029 <tomitaka1029@gmail.com>
2024-05-26 10:39:21 +0900

    Add detail product page

--- src/views/ProductDetail.vue
+++ src/views/ProductDetail.vue
@@ -1,5 +1,15 @@
 <template>
-  <h1>product detail</h1>
+  <button @click="router.push({ name: 'Catalog' })">カタログへ戻る</button>
+  <div class="product">
+    <div class="product-image">
+      <img :src="selectedProduct.thumbnail" alt="" />
+    </div>
+    <div class="product-details">
+      <p>ブランド: {{ selectedProduct.brand }}</p>
+      <p>詳細: {{ selectedProduct.description }}</p>
+      <h2>価格: {{ selectedProduct.price }}</h2>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,3 +19,28 @@ export default defineComponent({
   name: 'ProductDetail'
 })
 </script>
+
+<script setup>
+import { computed } from 'vue'
+import { productsStore } from '@/stores/products'
+import { useRoute, useRouter } from 'vue-router'
+
+const store = productsStore()
+const route = useRoute()
+const router = useRouter()
+
+const selectedProduct = computed(() => {
+  return store.products.find((item) => item.id === parseInt(route.params.id))
+})
+</script>
+
+<style scoped>
+.product {
+  display: flex;
+  margin-top: 50px;
+}
+
+.product-image {
+  margin-right: 24px;
+}
+</style>
